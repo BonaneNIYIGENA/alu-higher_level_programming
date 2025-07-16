d70bf20550de9ad66117e6299050189e56793b52
BonaneNIYIGENA <b.niyigena@alustudent.com>
2025-07-16 03:50:12 -0700

    Read and print out

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Module that defines a function to read a UTF-8 text file and print its contents."""
+"""Module that defines a function to read a UTF-8 text file
+and print its contents.
+"""
+
+
 def read_file(filename=""):
     """Reads a text file (UTF-8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
